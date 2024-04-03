f1806355a9feef962fb25d917e7725abb67aeccc
evil-89789 <tngsurya055@gmail.com>
2024-04-03 15:52:34 +0545

    event handlers

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function myFunction(a, b) {
 function calculateSimpleInterest(principal, rate, time) {
     return (principal * rate * time) / 100;
 }
+
+// creating an object 
+
+const person = {
+    firstName : "surya",
+    lastName : "tamang",
+    age : 20,
+
+    fullName : function(){
+        return "My name is " + this.firstName + " " + this.lastName;
+    }
+}
+
+
+document.write(person.fullName())
