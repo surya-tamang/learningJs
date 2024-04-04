10c1e7140665cdaf54d6388cc6cc2bb590a2afbb
evil-89789 <tngsurya055@gmail.com>
2024-04-04 07:03:46 +0545

    string methods

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 let interest = calculateSimpleInterest(5000, 5, 3);
 let forfunction = myFunction(2, 3);
-document.write(forfunction);
-document.write("Simple Interest:", interest);
+console.log(forfunction);
+console.log("Simple Interest:", interest);
 
 function myFunction(a, b) {
     return a + b;
@@ -16,14 +16,31 @@ function calculateSimpleInterest(principal, rate, time) {
 // creating an object 
 
 const person = {
-    firstName : "surya",
-    lastName : "tamang",
-    age : 20,
+    firstName: "surya",
+    lastName: "tamang",
+    age: 20,
 
-    fullName : function(){
+    fullName: function () {
         return "My name is " + this.firstName + " " + this.lastName;
     }
 }
 
+console.log(person.fullName())
 
-document.write(person.fullName())
+
+// basic string methods
+
+let text = "Text for basic string methods";
+
+let textLength = text.length;
+
+let letter = text[7];
+
+let string = text.substring(0, 14); // subString (start , end)
+
+
+console.log(text); // prints the string stored in variable text
+
+console.log(textLength); // prints the total number of letters of text variable
+console.log(letter); // prints the letter
+console.log(string) 
