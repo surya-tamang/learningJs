8187a8451ab42dd868bb42064a9423f2076d4b7a
evil-89789 <tngsurya055@gmail.com>
2024-04-05 08:32:52 +0545

    array

--- script.js
+++ script.js
@@ -30,17 +30,56 @@ console.log(person.fullName())
 
 // basic string methods
 
-let text = "Text for basic string methods";
+let text = "Text for basic string methods basic input";
 
 let textLength = text.length;
 
+let valid = text.includes("gmail","yahoo","hotmail");
+console.log(valid);
+
 let letter = text[7];
 
-let string = text.substring(0, 14); // subString (start , end)
+let string = text.substring(0, (text.length-2)); // subString (start , end)
 
 
 console.log(text); // prints the string stored in variable text
 
 console.log(textLength); // prints the total number of letters of text variable
 console.log(letter); // prints the letter
-console.log(string) 
+console.log(string)
+
+let stringmatch = text.matchAll(/basic/gi);
+
+console.log(Array.from(stringmatch));
+ let carName = "ford";
+
+ let carintro = `i hava a car named ${carName}`;
+
+ console.log(carintro);
+
+
+
+//  creating and array 
+
+const employess = ["ram", "shyam","hari"];
+console.log(employess[2]);
+
+const cars = new Array("ford", "volvo", "toyoto");
+cars.push("ferrari");
+console.log(cars);
+
+let list = "<ul>";
+cars.forEach((value)=>{
+list+= "<li>" + value + "</li>";
+})
+
+list += "</ul>";
+
+console.log(list);
+
+// let arrayToString = cars.toString();
+// console.log(arrayToString);
+
+let date = new Date();
+date.toString();
+console.log(date);
