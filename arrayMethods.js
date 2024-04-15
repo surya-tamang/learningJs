9bc46338709ef8176968b13788e18b3029ab9ab5
evil-89789 <tngsurya055@gmail.com>
2024-04-15 17:36:54 +0545

    array methods

---
+++ arrayMethods.js
@@ -0,0 +1,59 @@
+/*            \ [Array.map]  /              */
+
+const names = ['layla', 'miya', 'nana', 'kagura'];
+
+let touppercase = names.map(str => str.toUpperCase());
+
+console.log(touppercase);
+
+
+/*            \ [Array.filter]  /              */
+
+
+const num = [1, 2, 3, 4, 5];
+
+let onlyOdds = num.filter((el) => el % 2 != 0);
+
+console.log(onlyOdds);
+
+
+/*            \ [Array.Reduce]  /              */
+
+let sum = num.reduce((acc , n)=> acc +  n,0);
+console.log(sum);
+
+const strings = ['apple', 'banana', 'orange'];
+const longestString = strings.reduce((accumulator, currentValue) => {
+  return currentValue.length > accumulator.length ? currentValue : accumulator;
+// return accumulator + currentValue;
+}, '');
+console.log(longestString);
+
+
+/*            \ [Array.Findindex]  /              */
+
+const findarray = num.findIndex((el)=> el < 2 );
+console.log(findarray);
+
+/*            \ [Array.pop && push and shift && unshift]  /              */
+
+num.pop();  // removes the last element
+console.log(num);
+
+num.push(6); // add elements in last
+console.log(num);
+
+num.shift(); // removes the first element
+console.log(num);
+
+num.unshift(0); // add element in first
+console.log(num);
+
+
+/*            \ [Array.splice && slice]  /              */
+
+num.splice(num.length-1); // removes the element
+console.log(num);
+
+names.slice();
+console.log(names);
